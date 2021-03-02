43cd141d71013d6a1e4c0846ca2cf8d564b73430
周成 <zc@duiba.com.cn>
2021-03-02 14:09:21 +0800

    feat: tsx demo

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,10 @@ module.exports = {
       extends: ['./react.js']
     },
     {
+      files: ['demo/react.tsx'],
+      extends: ['./react.js', './typescript.js']
+    },
+    {
       files: ['demo/vue.vue'],
       extends: ['./vue.js']
     }
---
+++ demo/react.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  handleClick() {
+    console.log(1)
+  }
+
+  render() {
+    return <div onClick={this.handleClick}>Hello World</div>
+  }
+}
+
+export default App
